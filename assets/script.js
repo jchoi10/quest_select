37ba6fd46b7db083cf361a1fc8af8626d1b4deac
jchoi10 <noreply>
2022-10-26 00:37:38 -0400

    question array added

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,31 @@
+var questionSource = [
+    {
+        question: "1. Commonly used data types DO Not Include:",
+        choices: ["a. strings","b. booleans","c. alerts","d. numbers"],
+        answer: "c"
+    },
+    {
+        question: "2. Arrays in JavaScript can be used to store ________.",
+        choices: ["a. numbers and strings","b. other arrays","c. booleans","d. all of the above"],
+        answer: "d"
+    },
+    {
+        question: "3. A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["a. JavaScript","b. terminal/bash","c. for loops","d. console.log"],
+        answer: "d"
+    },
+    {
+        question: "4. The condition in an if / else statement is enclosed with _______.",
+        choices: ["a. quotes","b. curly brackets","c. parenthesis","d. square brackets"],
+        answer: "c"
+    },
+    {
+        question: "5. String values must be enclosed within _____ when being assigned to variables.",
+        choices: ["a. commas","b. curly brackets","c. quotes","d. parenthesis"],
+        answer: "c"
+    },
+]
+
 function countDown () {
     var timerInterval = setInterval(function(){
         secondLeft--;
